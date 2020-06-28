0f53bb61a7542cbc6eb79d133773fca47f2cca97
陈店店 <2242481718@qq.com>
2020-06-28 11:47:24 +0800

    第二次提交

---
+++ src/java0/run.java
@@ -0,0 +1,22 @@
+package java0;
+
+public class run extends Thread{
+	@Override
+	public void run() {
+		//TODO Auto-generated method stub
+		// super.run();
+		for (int i=0;i<2000;i++) {
+			System.out.println(i+ "+++多线程+++");
+		}
+	}
+
+	public static void main(String[] args) {
+		run thread =new run();
+		thread.start();
+		// TODO Auto-generated method stub
+for (int i=0; i<=2000; i++) {
+	System.out.println(i + "---主线方程---");
+}
+
+}
+}
